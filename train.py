badd18029ffd97dab4998088c73f065660fc02fd
Matt <matthillwakatipu@gmail.com>
2020-10-13 22:06:45 +1300

    Data generator version functioning

--- train.py
+++ train.py
@@ -20,6 +20,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras import backend as K
 
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import random
@@ -50,13 +51,16 @@ def construct_model():
     model = Sequential()
 
     # 3 x Convolutional layers with ReLU activation followed by max-pooling
-    model.add(Conv2D(32, (3, 3), input_shape=(300, 300, 3), activation='relu'))
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(300, 300, 3)))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(Conv2D(filters=32, kernel_size=(3, 3)))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3)))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())
@@ -66,9 +70,17 @@ def construct_model():
     model.add(Dense(3))
     model.add(Activation('sigmoid'))
 
+    # model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(300, 300, 3), activation='relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Flatten())
+    # model.add(Dense(3))
+    # model.add(Activation('softmax'))
+
     model.compile(loss='categorical_crossentropy',
-                  optimizer='sgd',
-                  metrics=['categorical_accuracy'])
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
 
     print(model.summary())
 
@@ -77,6 +89,7 @@ def construct_model():
 
 def create_data_generators():
     generator = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input)
+
     train_datagen = ImageDataGenerator(
         rescale=1. / 255,
         shear_range=0.2,
@@ -88,34 +101,33 @@ def create_data_generators():
     class_tuple = ['cherry', 'strawberry', 'tomato']
 
     train_generator = train_datagen.flow_from_directory(
-        directory='data/train',
-        target_size=image_size,
-        classes=class_tuple,)
-
-    validation_generator = test_datagen.flow_from_directory(
-        directory='data/validation',
-        target_size=image_size,
-        classes=class_tuple,)
-
-    test_generator = generator.flow_from_directory(
-        directory='data/test',
+        directory='train',
         target_size=image_size,
         classes=class_tuple,
+        batch_size=10)
+
+    validation_generator = test_datagen.flow_from_directory(
+        directory='validation',
+        target_size=image_size,
+        classes=class_tuple,
+        batch_size=10)
+
+    test_generator = generator.flow_from_directory(
+        directory='test',
+        target_size=image_size,
+        classes=class_tuple,
+        batch_size=10,
         shuffle=False)
 
-    print(train_generator.n)
-    print(validation_generator.n)
-    print(test_generator.n)
-
-    assert train_generator.n == 1000
-    assert validation_generator.n == 200
-    assert test_generator.n == 100
+    assert train_generator.n == 150
+    assert validation_generator.n == 30
+    assert test_generator.n == 15
     assert train_generator.num_classes == validation_generator.num_classes == test_generator.num_classes == 3
 
     return train_generator, validation_generator, test_generator
 
 
-def train_model(model):
+def train_model(model, train_generator, validation_generator):
     """
     Train the CNN model
     ***
@@ -124,12 +136,11 @@ def train_model(model):
     :param model: the initial CNN model
     :return:model:   the trained CNN model
     """
-    train_generator, validation_generator, test_generator = create_data_generators()
     model.fit(train_generator,
-              steps_per_epoch=2000,
+              steps_per_epoch=15,
               epochs=50,
               validation_data=validation_generator,
-              validation_steps=800,
+              validation_steps=3,
               verbose=1)
 
     # loss_and_metrics = model.evaluate(x_train, y_train, verbose=0)
@@ -143,6 +154,8 @@ def save_model(model):
     :param model: the trained CNN model
     :return:
     """
+    print(os.getcwd())
+    os.chdir('..')
     model.save("model/model.h5")
     print("Model Saved Successfully.")
 
@@ -153,6 +166,7 @@ def split_data():
     :return:
     """
     os.chdir('data')
+
     if os.path.isdir('train/cherry') is False:
         os.makedirs('train/cherry')
         os.makedirs('train/strawberry')
@@ -166,30 +180,46 @@ def split_data():
         os.makedirs('test/strawberry')
         os.makedirs('test/tomato')
 
-        for c in random.sample(glob.glob('cherry*'), 500):
+        for c in random.sample(glob.glob('cherry*'), 50):
             shutil.move(c, 'train/cherry')
-        for c in random.sample(glob.glob('strawberry*'), 500):
+        for c in random.sample(glob.glob('strawberry*'), 50):
             shutil.move(c, 'train/strawberry')
-        for c in random.sample(glob.glob('tomato*'), 500):
+        for c in random.sample(glob.glob('tomato*'), 50):
             shutil.move(c, 'train/tomato')
 
-        for c in random.sample(glob.glob('cherry*'), 100):
+        for c in random.sample(glob.glob('cherry*'), 10):
             shutil.move(c, 'validation/cherry')
-        for c in random.sample(glob.glob('strawberry*'), 100):
+        for c in random.sample(glob.glob('strawberry*'), 10):
             shutil.move(c, 'validation/strawberry')
-        for c in random.sample(glob.glob('tomato*'), 100):
+        for c in random.sample(glob.glob('tomato*'), 10):
             shutil.move(c, 'validation/tomato')
 
-        for c in random.sample(glob.glob('cherry*'), 50):
+        for c in random.sample(glob.glob('cherry*'), 5):
             shutil.move(c, 'test/cherry')
-        for c in random.sample(glob.glob('strawberry*'), 50):
+        for c in random.sample(glob.glob('strawberry*'), 5):
             shutil.move(c, 'test/strawberry')
-        for c in random.sample(glob.glob('tomato*'), 50):
+        for c in random.sample(glob.glob('tomato*'), 5):
             shutil.move(c, 'test/tomato')
+
+
+def plot_images(images_arr):
+    fig, axes = plt.subplots(1, 10, figsize=(20, 20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
     split_data()
+    train_generator, validation_generator, test_generator = create_data_generators()
+
+    images, labels = next(train_generator)
+    plot_images(images)
+    print(labels)
+
     model = construct_model()
-    model = train_model(model)
+    model = train_model(model, train_generator, validation_generator)
     save_model(model)
